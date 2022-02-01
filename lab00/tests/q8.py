b6d4250c4eb83fde2eb3666d846451a0fc8142ff
mahmoudharding <55882955+mahmoudharding@users.noreply.github.com>
2022-02-01 11:56:36 -0500

    Update lab00

---
+++ lab00/tests/q8.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q8',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> min_height_difference == 0.029999999999999805\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
